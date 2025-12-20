892ba01fa7ed4ecef275e1479b006868bf7a7cf9
linshangche <hi@nexai.info>
2025-12-20 08:45:51 +0800

    Add unified Noto Sans TC font

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-TW">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
